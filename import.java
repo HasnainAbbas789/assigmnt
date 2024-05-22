e8b1378d102e8740adba5316b1b04c510e506340
HasnainAbbas789 <hasnainabbas7787@gmail.com>
2024-05-22 23:33:40 +0500

    Add files via upload
    uploading  project to   github

---
+++ import.java
@@ -0,0 +1,14 @@
+import java.util;
+
+public class arrays{
+    public static void main(String[] args) {
+        int [] marks = new int [3];
+        marks[0] = 10; // phy
+        marks[1] = 64; // chem
+        marks[2] = 88; // eng   
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+
+}
